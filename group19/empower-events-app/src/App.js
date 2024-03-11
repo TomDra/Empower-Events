9fd09810e2d09f656d5910bddc4bb937ec82f1e2
MilesL283 <mileslegood205@gmail.com>
2024-03-11 17:54:44 +0000

    created login page without css styling, added axios and react-router-dom packages and added /login route

--- group19/empower-events-app/src/App.js
+++ group19/empower-events-app/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import HelloWorld from './HelloWorld';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HelloWorld from "./HelloWorld";
+import Login from "./components/pages/login";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+
   return (
-    <div>
-      <HelloWorld />
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HelloWorld />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
---
+++ group19/empower-events-app/src/components/pages/login.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+// Define the Login component
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Send a POST request to the server
+    try {
+      const response = await axios.post('http://localhost:3000/api/v1/auth/login/', {
+        email,
+        password,
+      });
+
+      // Log success
+      console.log('Login successful:', response.data);
+    } catch (error) {
+      // Log failure
+      console.error('Login failed:', error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
