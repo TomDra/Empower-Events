6cd992446a56b2e580c124c52b795f09fd066bdb
Samuel Ivuerah <73389585+sam-iv@users.noreply.github.com>
2024-03-26 17:02:54 +0000

    Created an API endpoint for user registration. (UserAPI)
    - Changed the structure of the validators for UserAPI to accommodate more validators.
    - Refactored and added Tests in tests.py to test new functionality.
    - Better Error Handling in views.py.
    - Fixed some errors (settings.py & views.py)

--- group19/UserAPI/urls.py
+++ group19/UserAPI/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('login/', views.UserLogin.as_view(), name='login'),
     path('logout/', views.UserLogout.as_view(), name='logout'),
+    path('register/', views.UserRegister.as_view(), name='register')
 ]
