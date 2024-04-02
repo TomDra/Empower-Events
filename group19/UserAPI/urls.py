b365cb8f1c53ae72a9216450ddaa068059d48f19
Tom Draper <drapt002@gmail.com>
2024-04-03 00:27:36 +0100

    Removed merge head

--- group19/UserAPI/urls.py
+++ group19/UserAPI/urls.py
@@ -5,9 +5,6 @@ from . import views
 urlpatterns = [
     path('login/', views.UserLogin.as_view(), name='login'),
     path('logout/', views.UserLogout.as_view(), name='logout'),
-<<<<<<< HEAD
-    path('register/', views.UserRegister.as_view(), name='register')
-=======
->>>>>>> refs/remotes/origin/5-make-home-page
+    path('register/', views.UserRegister.as_view(), name='register'),
     path('user', views.UserView.as_view(), name='user'),
 ]
