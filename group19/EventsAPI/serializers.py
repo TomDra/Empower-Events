8352a3003c9e7d81a62c87e708e5c3d4c923eb9a
Samuel Ivuerah <73389585+sam-iv@users.noreply.github.com>
2024-04-12 16:41:33 +0100

    Updated serializers.py and tests.py - Changed the return JSON to contained requested data with the correct structure. - Made tests.py more extensive.

--- group19/EventsAPI/serializers.py
+++ group19/EventsAPI/serializers.py
@@ -1,41 +1,111 @@
-# Importing necessary libraries/modules from Django and rest_framework.
+# Importing necessary libraries and modules
+import json
+
 from rest_framework import serializers
-from myapi.models import Calendar, Activity, ActivityLeader
+
+from myapi.models import Calendar, Activity, Charity, AgeGroup
 
 
-# TODO: Adjust the serializers to return more information about the models.
+class CharitySerializer(serializers.ModelSerializer):
+    """
+    CharitySerializer class is a subclass of ModelSerializer that serializes the Charity model.
+    It contains the following fields:
+    - charity_name: A CharField that represents the name of the charity.
+    """
+
+    class Meta:
+        model = Charity
+        fields = ['charity_name']
+
+
+class AgeGroupSerializer(serializers.ModelSerializer):
+    """
+    AgeGroupSerializer class is a subclass of ModelSerializer that serializes the AgeGroup model.
+    It contains the following fields:
+    - group_title: A CharField that represents the title of the age group.
+    """
+
+    class Meta:
+        model = AgeGroup
+        fields = ['group_title']
+
+
 class ActivitySerializer(serializers.ModelSerializer):
     """
     ActivitySerializer class is a subclass of ModelSerializer that serializes the Activity model.
-    It contains all fields from the expected Activity model.
+
+    It contains the following fields:
+    - description: A CharField that represents the description of the activity.
+    - latitude: A DecimalField that represents the latitude of the location of the activity.
+    - longitude: A DecimalField that represents the longitude of the location of the activity.
+    - compatible_disabilities: A JSONField that represents the compatible disabilities of the activity.
+    - charity: The CharitySerializer that represents the charity of the activity.
+    - age_group: The AgeGroupSerializer that represents the age group of the activity.
+    - activity_id: An AutoField that represents the ID of the activity.
     """
+    charity = CharitySerializer(read_only=True)
+    age_group = AgeGroupSerializer(read_only=True)
 
     class Meta:
         model = Activity
-        fields = '__all__'
+        fields = ['description', 'latitude', 'longitude', 'compatible_disabilities', 'charity', 'age_group',
+                  'activity_id']
 
+    def to_representation(self, instance):
+        """
+        A method to serialize the representation of the activity in a custom way.
 
-class ActivityLeaderSerializer(serializers.ModelSerializer):
-    """
-    ActivityLeaderSerializer class is a subclass of ModelSerializer that serializes the ActivityLeader model.
-    It contains all fields from the expected ActivityLeader model.
-    """
+        :param instance: The instance of the activity.
+        :return: The serialized representation of the activity.
+        """
 
-    class Meta:
-        model = ActivityLeader
-        fields = '__all__'
+        # Serialize the instance
+        representation = super().to_representation(instance)
+
+        # Update the representation
+        representation['charity'] = representation['charity']['charity_name']
+        representation['age_group'] = representation['age_group']['group_title']
+
+        # Load the compatible disabilities from JSON
+        representation['compatible_disabilities'] = json.loads(representation['compatible_disabilities'])
+
+        # Return the updated representation
+        return representation
 
 
 class CalendarSerializer(serializers.ModelSerializer):
     """
     CalendarSerializer class is a subclass of ModelSerializer that serializes the Calendar model.
-    It contains all fields from the expected Calendar model.
+
+    It contains the following fields:
+    - activity: The ActivitySerializer that represents the activity of the calendar event.
+    - time: A DateTimeField that represents the time of the calendar event.
+    - event_id: An AutoField that represents the ID of the calendar event.
     """
 
-    # Serializing the activity and activity_leader fields.
     activity = ActivitySerializer()
-    activity_leader = ActivityLeaderSerializer()
 
     class Meta:
         model = Calendar
-        fields = '__all__'
+        fields = ['activity', 'time', 'event_id']
+
+    def to_representation(self, instance):
+        """
+        A method to serialize the representation of the calendar event in a custom way.
+
+        :param instance: The instance of the calendar event.
+        :return: The serialized representation of the calendar event.
+        """
+
+        # Serialize the instance
+        representation = super().to_representation(instance)
+
+        # Update the representation
+        representation['date'] = representation.pop('time')
+
+        # Load and update the activity representation
+        activity_representation = representation.pop('activity')
+        representation.update(activity_representation)
+
+        # Return the updated representation
+        return representation
--- group19/EventsAPI/tests.py
+++ group19/EventsAPI/tests.py
@@ -23,33 +23,45 @@ class EventsAPITest(TestCase):
         """
         self.client = APIClient()
 
+        # Create users to simulate activity leaders.
         self.user1 = User.objects.create_user(username='user1', password='testpass123', email='user1@email.com')
         self.user2 = User.objects.create_user(username='user2', password='testpass123', email='user2@email.com')
 
+        # Create charities
         self.charity1 = Charity.objects.create(charity_name='Charity 1', email='charity1@email.com',
                                                password='testpass123')
         self.charity2 = Charity.objects.create(charity_name='Charity 2', email='charity2@email.com',
                                                password='testpass123')
 
-        self.age_group1 = AgeGroup.objects.create(age_range_lower=10, age_range_higher=20, group_title='Group 1')
-        self.age_group2 = AgeGroup.objects.create(age_range_lower=20, age_range_higher=30, group_title='Group 2')
+        # Create age groups
+        self.age_group1 = AgeGroup.objects.create(age_range_lower=10, age_range_higher=20, group_title='Test Group 1')
+        self.age_group2 = AgeGroup.objects.create(age_range_lower=20, age_range_higher=30, group_title='Test Group 2')
 
+        # Create activity leaders
         self.activity_leader1 = ActivityLeader.objects.create(user=self.user1, name='Test Leader 1',
                                                               birth_date=timezone.now(), charity=self.charity1)
         self.activity_leader2 = ActivityLeader.objects.create(user=self.user2, name='Test Leader 2',
                                                               birth_date=timezone.now(), charity=self.charity2)
 
+        # Create activities
         self.activity1 = Activity.objects.create(description='Test Activity 1', latitude=0.0, longitude=0.0,
                                                  charity=self.charity1, age_group=self.age_group1)
+        self.activity1.set_compatible_disabilities(["Test Disability 1", "Test Disability 2"])
+        self.activity1.save()
         self.activity2 = Activity.objects.create(description='Test Activity 2', latitude=0.0, longitude=0.0,
                                                  charity=self.charity2, age_group=self.age_group2)
+        self.activity2.set_compatible_disabilities(["Test Disability 3", "Test Disability 4"])
+        self.activity2.save()
 
+        # Create calendars (whilst setting the expected date strings to ISO 8601 format)
         self.calendar1 = Calendar.objects.create(activity=self.activity1,
                                                  time=timezone.now() + datetime.timedelta(days=1),
                                                  activity_leader=self.activity_leader1)
+        self.expected_date1 = self.calendar1.time.isoformat(timespec='microseconds').replace('+00:00', 'Z')
         self.calendar2 = Calendar.objects.create(activity=self.activity2,
                                                  time=timezone.now() + datetime.timedelta(days=2),
                                                  activity_leader=self.activity_leader2)
+        self.expected_date2 = self.calendar2.time.isoformat(timespec='microseconds').replace('+00:00', 'Z')
 
 
 class UpcomingEventsListTest(EventsAPITest):
@@ -80,13 +92,25 @@ class UpcomingEventsListTest(EventsAPITest):
         self.assertIsNone(response.data['previous'])
 
         # Check the first event in the list
-        # TODO: Make it extensive.
         event1 = response.data['results'][0]
-        self.assertEqual(event1['activity']['description'], 'Test Activity 1')
-        self.assertEqual(event1['activity_leader']['name'], 'Test Leader 1')
+        self.assertEqual(event1['date'], self.expected_date1)
+        self.assertEqual(event1['description'], 'Test Activity 1')
+        self.assertEqual(event1['latitude'], '0.000000')
+        self.assertEqual(event1['longitude'], '0.000000')
+        self.assertEqual(event1['compatible_disabilities'], ['Test Disability 1', 'Test Disability 2'])
+        self.assertEqual(event1['charity'], 'Charity 1')
+        self.assertEqual(event1['age_group'], 'Test Group 1')
+        self.assertEqual(event1['activity_id'], self.activity1.activity_id)
+        self.assertEqual(event1['event_id'], self.calendar1.event_id)
 
         # Check the second event in the list
-        # TODO: Make it extensive.
         event2 = response.data['results'][1]
-        self.assertEqual(event2['activity']['description'], 'Test Activity 2')
-        self.assertEqual(event2['activity_leader']['name'], 'Test Leader 2')
+        self.assertEqual(event2['date'], self.expected_date2)
+        self.assertEqual(event2['description'], 'Test Activity 2')
+        self.assertEqual(event2['latitude'], '0.000000')
+        self.assertEqual(event2['longitude'], '0.000000')
+        self.assertEqual(event2['compatible_disabilities'], ['Test Disability 3', 'Test Disability 4'])
+        self.assertEqual(event2['charity'], 'Charity 2')
+        self.assertEqual(event2['age_group'], 'Test Group 2')
+        self.assertEqual(event2['activity_id'], self.activity2.activity_id)
+        self.assertEqual(event2['event_id'], self.calendar2.event_id)
