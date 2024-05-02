7e0dc9cc6ac4e43e763e1aece7f37ff0e6214eae
omar houchou <oh00394@surrey.ac.uk>
2024-05-02 18:38:44 +0100

    done

--- group19/empower-events-app/src/App.js
+++ group19/empower-events-app/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import AppNavbar from './components/navbar';
 import Login from './components/pages/login';
 import SignUp from './components/pages/signup';
+import HomePage from './components/pages/homePage';
 import { UserProvider } from './contexts/userContext';
 
 
@@ -24,9 +25,10 @@ const App = () => {
     <BrowserRouter>
       <AppNavbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login onLogin={handleLogin} />} />
         <Route path="/register" element={<SignUp />} />
-
+        <Route path="/test" element={<HomePage />} />
         {/* Add other routes as needed */}
       </Routes>
     </BrowserRouter>
