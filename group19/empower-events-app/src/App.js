9a56f0259a0fc16d17d088e09b86468e062c8af5
Tom Draper <drapt002@gmail.com>
2024-05-11 19:45:02 +0100

    Added charity nav bar

--- group19/empower-events-app/src/App.js
+++ group19/empower-events-app/src/App.js
@@ -1,24 +1,19 @@
-//import React from "react";
-import axios from "axios";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from "./components/footer";
+import { UserProvider } from './contexts/userContext';
+import HomePage from "./components/pages/homePage";
 import Login from "./components/pages/login";
 import SignUp from "./components/pages/signup";
-import HomePage from "./components/pages/homePage";
 import Events from "./components/pages/events";
 import Feedback from "./components/pages/feedback";
 import AdminPortal from "./components/pages/charityPortal";
 import AdminEvents from "./components/pages/viewFeedbackList";
 import AdminFeedback from "./components/pages/viewFeedbackDetail";
 import CharityLogin from "./components/pages/charityLogIn";
-import "./App.css";
 import EventDetailPage from "./components/pages/eventdetails";
-// src/App.js
-import React, { useEffect, useState } from 'react';
-//import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import AppNavbar from './components/navbar';
-import Footer from "./components/footer";
-import { UserProvider } from './contexts/userContext';
-
+import NavigationBar from "./components/NavigationBar";
+import "./App.css";
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -32,30 +27,30 @@ const App = () => {
   };
 
   return (
-  <div className="App">
-    <UserProvider>
-    <BrowserRouter>
-      <AppNavbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
-      <div className="content-wrapper">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login onLogin={handleLogin} />} />
-        <Route path="/register" element={<SignUp />} />
-        <Route path="/charity/login" element={<CharityLogin onLogin={handleLogin}/>} />
-        <Route path="/test" element={<HomePage />} />
-        <Route path="/events/:eventId" element={<EventDetailPage />} />
-        <Route key="past" path="/events/past" element={<Events />} />
-        <Route key="future" path="/events/future" element={<Events />} />
-        <Route path="/feedback/:eventId" element={<Feedback />} />
-        <Route path="/admin/portal" element={<AdminPortal />} />
-        <Route path="/admin/events" element={<AdminEvents />} />
-        <Route path="/admin/feedback/:eventId" element={<AdminFeedback />} />
-        {/* Add other routes as needed */}
-      </Routes>
-      </div>
-      <Footer />
-    </BrowserRouter>
-    </UserProvider>
+    <div className="App">
+      <UserProvider>
+        <BrowserRouter>
+          <NavigationBar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+          <div className="content-wrapper">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<Login onLogin={handleLogin} />} />
+              <Route path="/register" element={<SignUp />} />
+              <Route path="/charity/login" element={<CharityLogin onLogin={handleLogin}/>} />
+              <Route path="/test" element={<HomePage />} />
+              <Route path="/events/:eventId" element={<EventDetailPage />} />
+              <Route key="past" path="/events/past" element={<Events />} />
+              <Route key="future" path="/events/future" element={<Events />} />
+              <Route path="/feedback/:eventId" element={<Feedback />} />
+              <Route path="/admin/portal" element={<AdminPortal />} />
+              <Route path="/admin/events" element={<AdminEvents />} />
+              <Route path="/admin/feedback/:eventId" element={<AdminFeedback />} />
+              {/* Add other routes as needed */}
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </UserProvider>
     </div>
   );
 };
