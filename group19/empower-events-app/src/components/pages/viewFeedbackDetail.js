46831fc8eba5b60ec27031f3e8577855fecedadd
Tom Draper <drapt002@gmail.com>
2024-05-13 02:31:17 +0100

    added bar graph for question responses

--- group19/empower-events-app/src/components/pages/viewFeedbackDetail.js
+++ group19/empower-events-app/src/components/pages/viewFeedbackDetail.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { ArcElement, RadialLinearScale } from 'chart.js';
+import { PolarArea } from 'react-chartjs-2';
+
+
 import {
   Chart as ChartJS,
-  ArcElement,
   CategoryScale,
-  RadialLinearScale,
-  Tooltip,
-} from "chart.js";
-import { PolarArea } from "react-chartjs-2";
+  LinearScale,
+  Title,
+  BarElement,
+  Tooltip
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  Title,
+  BarElement,
+  Tooltip
+);
+
+
 ChartJS.register(ArcElement, CategoryScale, RadialLinearScale, Tooltip);
 
 const AdminFeedback = () => {
@@ -19,6 +34,7 @@ const AdminFeedback = () => {
   const [questions, setQuestions] = useState({});
   const { eventId } = useParams();
   const [sentimentData, setSentimentData] = useState({});
+  const [questionChartData, setQuestionChartData] = useState(null);
 
   useEffect(() => {
     const getData = async () => {
@@ -44,10 +60,7 @@ const AdminFeedback = () => {
         setResponseData(responseOverview.data);
         setActivityFeedback(responseActivity.data.results);
         setLeaderFeedback(responseLeader.data.results);
-        //setQuestions(responseQuestions.data.questions);
         setQuestions(responseQuestions.data.questions);
-        console.log(responseQuestions.data.questions);
-
 
         const sentimentData = {
           labels: [
@@ -75,6 +88,50 @@ const AdminFeedback = () => {
         };
 
         setSentimentData(sentimentData);
+
+        // Prepare data for question chart
+        const questionLabels = Object.keys(responseQuestions.data.questions);
+        const questionData = questionLabels.map((label) => {
+          const { positive, negative } = responseQuestions.data.questions[label];
+          return {
+            label,
+            positive: positive,
+            negative: negative
+          };
+        });
+
+        setQuestionChartData({
+          labels: questionLabels,
+          datasets: [
+            {
+              label: "Positive",
+              backgroundColor: "rgba(75,192,192,0.4)",
+              borderColor: "rgba(75,192,192,1)",
+              borderWidth: 1,
+              hoverBackgroundColor: "rgba(75,192,192,0.6)",
+              hoverBorderColor: "rgba(75,192,192,1)",
+              data: questionData.map((data) => data.positive)
+            },
+            {
+              label: "Negative",
+              backgroundColor: "rgba(255,99,132,0.4)",
+              borderColor: "rgba(255,99,132,1)",
+              borderWidth: 1,
+              hoverBackgroundColor: "rgba(255,99,132,0.6)",
+              hoverBorderColor: "rgba(255,99,132,1)",
+              data: questionData.map((data) => data.negative)
+            },
+            {
+              label: "Midpoint",
+              backgroundColor: "rgba(255,205,86,0.4)",
+              borderColor: "rgba(255,205,86,1)",
+              borderWidth: 1,
+              hoverBackgroundColor: "rgba(255,205,86,0.6)",
+              hoverBorderColor: "rgba(255,205,86,1)",
+              data: questionData.map((data) => data.midpoint)
+            },
+          ]
+        });
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -128,22 +185,30 @@ const AdminFeedback = () => {
                 </div>
               </div>
               <h3>Question responses</h3>
-              {Object.keys(questions).map((question) => (
-                <div className="card mb-3 text-start">
-                  <h5 className="card-title">{question}</h5>
-                  <div className="card-body">
-                    <p>
-                      Positive: {questions[question].positive}
-                    </p>
-                    <p>
-                      Negative: {questions[question].negative}
-                    </p>
-                    <p>
-                      Midpoint: {questions[question].midpoint}
-                    </p>
-                  </div>
+              <div className="card mb-3 text-start">
+                <div className="card-body">
+                  <Bar
+                      data={questionChartData}
+                      width={100}
+
+                      height={200}
+                      options={{
+                        maintainAspectRatio: false,
+                        scales: {
+                          x: {
+                            type: 'category',
+                            stacked: false
+                          },
+                          y: {
+                            type: 'linear',
+                            stacked: false
+                          }
+                        }
+                      }}
+                    />
+
                 </div>
-              ))}
+              </div>
               <h3>Audio responses</h3>
               <div className="card mb-3 text-start">
                 <div className="card-body d-flex flex-column align-items-center">
